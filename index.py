152185cd711b8c63c47f2093e77987af1fcd4481
Elroy Hui <huielroy@hotmail.com>
2022-06-26 19:26:59 -0400

    ++ compiles an array of every link to every UFC event on record

--- index.py
+++ index.py
@@ -1,13 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 URL = "http://ufcstats.com/statistics/events/completed?page=all"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
+data = []
+links = []
+
 
 # print(soup)
 
 for link in soup.find_all('a'):
-  print(link.get('href'))
+  href = str(link.get('href'))
+  match = re.search("(?P<url>https?://ufcstats.com/event-details/.+)", href)
+  if match is not None:
+    links.append((match.group("url")))
+
+
+print(links)
 
