bb4bdb75b91df2cd4b1b3bcc0557d3ceafcae88f
Elroy Hui <huielroy@hotmail.com>
2022-06-25 23:51:03 -0400

    changed target URL

--- index.py
+++ index.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "http://ufcstats.com/statistics/fighters"
+URL = "http://ufcstats.com/statistics/events/completed?page=all"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-print(soup)
+# print(soup)
+
+for link in soup.find_all('a'):
+  print(link.get('href'))
+
