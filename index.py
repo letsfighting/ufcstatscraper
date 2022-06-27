9800d7a111aa89aa05de799138ad856f0efc4d04
Elroy Hui <huielroy@hotmail.com>
2022-06-26 21:22:56 -0400

    ++now puts all fight URLs into an array

--- index.py
+++ index.py
@@ -7,7 +7,8 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 data = []
-links = []
+eventlinks = []
+fightlinks = []
 
 
 # print(soup)
@@ -16,8 +17,19 @@ for link in soup.find_all('a'):
   href = str(link.get('href'))
   match = re.search("(?P<url>https?://ufcstats.com/event-details/.+)", href)
   if match is not None:
-    links.append((match.group("url")))
+    eventlinks.append((match.group("url")))
 
 
-print(links)
+# print(links)
 
+for event in eventlinks:
+  eventpage = requests.get(event)
+  soup = BeautifulSoup(eventpage.content, "html.parser")
+  for fightlink in soup.find_all('a'):
+    href = str(fightlink.get('href'))
+    match = re.search("(?P<url>https?://ufcstats.com/fight-details/.+)", href)
+    if match is not None:
+      fightlinks.append((match.group("url")))
+
+
+print(fightlinks)
