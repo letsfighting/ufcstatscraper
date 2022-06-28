bbbe846abd78df94f5fb5788cf7704003e55b972
Elroy Hui <huielroy@hotmail.com>
2022-06-28 17:23:17 -0400

    added a way to retrieve event names in first loop

--- index.py
+++ index.py
@@ -8,28 +8,33 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 data = []
 eventlinks = []
+eventnames = []
 fightlinks = []
 
 
 # print(soup)
 
 for link in soup.find_all('a'):
+  text = str(link.get_text())
+  text = text.strip()
   href = str(link.get('href'))
   match = re.search("(?P<url>https?://ufcstats.com/event-details/.+)", href)
   if match is not None:
     eventlinks.append((match.group("url")))
+    eventnames.append(text)
 
 
-# print(links)
 
-for event in eventlinks:
-  eventpage = requests.get(event)
-  soup = BeautifulSoup(eventpage.content, "html.parser")
-  for fightlink in soup.find_all('a'):
-    href = str(fightlink.get('href'))
-    match = re.search("(?P<url>https?://ufcstats.com/fight-details/.+)", href)
-    if match is not None:
-      fightlinks.append((match.group("url")))
+print(eventnames)
+
+# for event in eventlinks:
+#   eventpage = requests.get(event)
+#   soup = BeautifulSoup(eventpage.content, "html.parser")
+#   for fightlink in soup.find_all('a'):
+#     href = str(fightlink.get('href'))
+#     match = re.search("(?P<url>https?://ufcstats.com/fight-details/.+)", href)
+#     if match is not None:
+#       fightlinks.append((match.group("url")))
 
 
 # print(fightlinks)
