f77f418dc5399469287c2fdb924bfe1cbc849222
Elroy Hui <huielroy@hotmail.com>
2022-07-06 12:57:41 -0400

    found a way to fetch event name from fight details page

--- fightdetails.py
+++ fightdetails.py
@@ -20,14 +20,15 @@ soup = BeautifulSoup(page.content, "html.parser")
 # fightdata = []
 # fighters = []
 
-fighterLinks = []
-fighterNames = []
+fighter_links = []
+fighter_names = []
 stats = []
 ref = []
 details = []
 labels = []
 data = []
-winnersLosers = []
+winners_losers = []
+fight = []
 
 # print(soup)
 
@@ -37,14 +38,14 @@ for link in soup.find_all('i', {"class": "b-fight-details__person-status"}):
   # href = str(link.get('href'))
   # match = re.search("(?P<url>https?://ufcstats.com/fighter-details/.+)", href)
   # if match is not None:
-  winnersLosers.append(text)
+  winners_losers.append(text)
 
 # # for x in range(0, len(eventlinks)):
 # #   event = {'_id': len(eventlinks)-x, 'name': eventnames[x], 'url': eventlinks[x]}
 # #   eventdata.append(event)
 
 
-print(f"winnerLosers: {winnersLosers}")
+print(f"winnerLosers: {winners_losers}")
 
 for link in soup.find_all('a'):
   text = str(link.get_text())
@@ -52,35 +53,35 @@ for link in soup.find_all('a'):
   href = str(link.get('href'))
   match = re.search("(?P<url>https?://ufcstats.com/fighter-details/.+)", href)
   if match is not None:
-    fighterLinks.append((match.group("url")))
-    fighterNames.append(text)
+    fighter_links.append((match.group("url")))
+    fighter_names.append(text)
 
 # # for x in range(0, len(eventlinks)):
 # #   event = {'_id': len(eventlinks)-x, 'name': eventnames[x], 'url': eventlinks[x]}
 # #   eventdata.append(event)
 
 
-fighterLinks = list(dict.fromkeys(fighterLinks))
-fighterNames = list(dict.fromkeys(fighterNames))
+fighter_links = list(dict.fromkeys(fighter_links))
+fighter_names = list(dict.fromkeys(fighter_names))
 
-print(f"Fighter Links: {fighterLinks}")
-print(f"Fighter Names: {fighterNames}")
+print(f"Fighter Links: {fighter_links}")
+print(f"Fighter Names: {fighter_names}")
 
 for link in soup.find_all('p', {"class": "b-fight-details__table-text"}):
   text = str(link.get_text())
   text = text.strip()
-  if (text != fighterNames[0] and text != fighterNames[1]):
+  if (text != fighter_names[0] and text != fighter_names[1]):
     stats.append((text))
 
 print(f"stats: {stats}")
 
-for link in soup.find_all('span'):
-  text = str(link.get_text())
-  text = text.strip()
-  ref.append((text))
+# for link in soup.find_all('span'):
+#   text = str(link.get_text())
+#   text = text.strip()
+#   ref.append((text))
 
 
-print(f"ref: {ref}")
+# print(f"ref: {ref}")
 
 for link in soup.find_all('i', {"class": "b-fight-details__text-item"}):
   text = str(link.get_text())
@@ -117,7 +118,21 @@ print(f"labels: {labels}")
 print(f"data: {data}")
 
 
-#fight details organization
+
+for link in soup.find_all('a'):
+  text = str(link.get_text())
+  text = text.strip()
+  href = str(link.get('href'))
+  match = re.search("(?P<url>https?://ufcstats.com/event-details/.+)", href)
+  if match is not None:
+    fight.append(text)
+
+
+print(f"fight: {fight}")
+
+
+# fight details organization
+
 
 
 
