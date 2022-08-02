ed716cfecd2af07a0b784ca7b70492d050b43768
Elroy Hui <huielroy@hotmail.com>
2022-08-02 17:13:44 -0400

    working fighters.py adding the bridging general profile data to fighters collection

--- fighters.py
+++ fighters.py
@@ -8,9 +8,8 @@ from pymongo import MongoClient
 patch_all()
 
 
-fightername = []
-fighterstat = []
 
+fighters = []
 
 # print(soup)
 
@@ -20,9 +19,9 @@ fighterstat = []
 client = MongoClient()
 
 #  START
-# for fighterlink in client.ufcstats.fighterlinks.find():
-#   fighterpage = requests.get(fighterlink['url'])
-#   soup = BeautifulSoup(fighterpage.content, "html.parser")
+for fighterlink in client.ufcstats.fighterlinks.find():
+  fighterpage = requests.get(fighterlink['url'])
+  soup = BeautifulSoup(fighterpage.content, "html.parser")
 
 #   for name in soup.find_all('span', {"class": "b-content__title-highlight"}):
 #     text = str(name.get_text())
@@ -41,57 +40,66 @@ client = MongoClient()
 #   soup = BeautifulSoup(fighterpage.content, "html.parser")
 
 # fighterpage = requests.get('http://ufcstats.com/fighter-details/93fe7332d16c6ad9')
-fighterpage = requests.get('http://ufcstats.com/fighter-details/3329d692aea4dc28')
-soup = BeautifulSoup(fighterpage.content, "html.parser")
+# fighterpage = requests.get('http://ufcstats.com/fighter-details/3329d692aea4dc28')
+# soup = BeautifulSoup(fighterpage.content, "html.parser")
 
 
-for name in soup.find_all('span', {"class": "b-content__title-highlight"}):
-  text = str(name.get_text())
-  text = text.strip()
-  fightername.append(text)
-for attribute in soup.find_all('li', {"class": "b-list__box-list-item_type_block"}):
-  text = str(attribute.get_text())
-  text = text.strip()
-  trimmed = re.search("(?s).*?Height:(.*)", text)
-  if trimmed is not None:
-    feet = ((trimmed.group(1)[:-4].strip()))
-    inches = ((trimmed.group(1)[-2].strip()))
-    if feet.isnumeric() and inches.isnumeric():
-      totalheight = (int(feet) * 12 + int(inches))
-      fighterstat.append(totalheight)
-    else:
-      fighterstat.append(trimmed.group(1).strip())
+  fightername = []
+  fighterstat = []
 
-  trimmed = re.search("(?s).*Weight:(.*)", text)
-  if trimmed is not None:
-    if trimmed.group(1)[:-4].strip().isnumeric():
-      fighterstat.append(int(trimmed.group(1)[:-4].strip()))
-    else:
-      fighterstat.append(trimmed.group(1).strip())
-  trimmed = re.search("(?s).*Reach:(.*)", text)
-  if trimmed is not None:
-    if  trimmed.group(1)[:-1].strip().isnumeric():
-      fighterstat.append(int(trimmed.group(1)[:-1].strip()))
-    else:
-      fighterstat.append('--')
-  trimmed = re.search("(?s).*STANCE:(.*)", text)
-  if trimmed is not None:
-    if trimmed.group(1).strip():
-      fighterstat.append((trimmed.group(1).strip()))
-    else:
-      fighterstat.append('--')
+  for name in soup.find_all('span', {"class": "b-content__title-highlight"}):
+    text = str(name.get_text())
+    text = text.strip()
+    fightername.append(text)
 
-  trimmed = re.search("(?s).*DOB:(.*)", text)
-  if trimmed is not None:
-    if trimmed.group(1)[-4:].strip().isnumeric():
-      fighterstat.append(int(trimmed.group(1)[-4:].strip()))
-    else:
-      fighterstat.append('--')
+  for attribute in soup.find_all('li', {"class": "b-list__box-list-item_type_block"}):
+    text = str(attribute.get_text())
+    text = text.strip()
+    trimmed = re.search("(?s).*?Height:(.*)", text)
+    if trimmed is not None:
+      feet = ((trimmed.group(1)[:-4].strip()))
+      inches = ((trimmed.group(1)[-2].strip()))
+      if feet.isnumeric() and inches.isnumeric():
+        totalheight = (int(feet) * 12 + int(inches))
+        fighterstat.append(totalheight)
+      else:
+        fighterstat.append(trimmed.group(1).strip())
+
+    trimmed = re.search("(?s).*Weight:(.*)", text)
+    if trimmed is not None:
+      if trimmed.group(1)[:-4].strip().isnumeric():
+        fighterstat.append(int(trimmed.group(1)[:-4].strip()))
+      else:
+        fighterstat.append(trimmed.group(1).strip())
+    trimmed = re.search("(?s).*Reach:(.*)", text)
+    if trimmed is not None:
+      if  trimmed.group(1)[:-1].strip().isnumeric():
+        fighterstat.append(int(trimmed.group(1)[:-1].strip()))
+      else:
+        fighterstat.append('--')
+    trimmed = re.search("(?s).*STANCE:(.*)", text)
+    if trimmed is not None:
+      if trimmed.group(1).strip():
+        fighterstat.append((trimmed.group(1).strip()))
+      else:
+        fighterstat.append('--')
+
+    trimmed = re.search("(?s).*DOB:(.*)", text)
+    if trimmed is not None:
+      if trimmed.group(1)[-4:].strip().isnumeric():
+        fighterstat.append(int(trimmed.group(1)[-4:].strip()))
+      else:
+        fighterstat.append('--')
 
 
+  fighterprofile = {'_id': (re.search("(?s).*?fighter-details/(.*)", fighterlink['url'])).group(1), 'name': fightername[0], 'height': fighterstat[0], 'weight': fighterstat[1], 'reach': fighterstat[2], 'stance': fighterstat[3], 'dob': fighterstat[4]}
 
-print(fightername)
-print(fighterstat)
+  fighters.append(fighterprofile)
+
+  print(f"Fighter: {fightername[0]} scanned.")
+
+print(fighters)
+
 # END
 
 
@@ -109,8 +117,8 @@ print(fighterstat)
 
 # print(fightdata)
 
-# client.ufcstats.fighterlinks.drop()
-# client.ufcstats.fighterlinks.insert_many(fighterdata)
+# client.ufcstats.fighters.drop()
+client.ufcstats.fighters.insert_many(fighters)
 
 
  #
