8454e9e2b1e79ef29386054cc46f5878bc4d8fe4
Elroy Hui <huielroy@hotmail.com>
2022-08-02 16:22:20 -0400

    wip fighters.py currently works on a single page to extract general fighter info about height weight age stance reach etc.

---
+++ fighters.py
@@ -0,0 +1,110 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import pymongoarrow as pma
+from pymongoarrow.monkey import patch_all
+from pymongo import MongoClient
+
+patch_all()
+
+
+fightername = []
+fighterstat = []
+
+
+# print(soup)
+
+
+
+
+client = MongoClient()
+
+#  START
+# for fighterlink in client.ufcstats.fighterlinks.find():
+#   fighterpage = requests.get(fighterlink['url'])
+#   soup = BeautifulSoup(fighterpage.content, "html.parser")
+
+#   for name in soup.find_all('span', {"class": "b-content__title-highlight"}):
+#     text = str(name.get_text())
+#     text = text.strip()
+#     fightername.append(text)
+
+
+#   for attribute in soup.find_all('li', {"class": "b-list__box-list-item_type_block"}):
+#     text = str(attribute.get_text())
+#     text = text.strip()
+#     fighterstat.append((text))
+
+
+# for fighterlink in client.ufcstats.fighterlinks.find():
+#   fighterpage = requests.get(fighterlink['url'])
+#   soup = BeautifulSoup(fighterpage.content, "html.parser")
+
+fighterpage = requests.get('http://ufcstats.com/fighter-details/3329d692aea4dc28')
+soup = BeautifulSoup(fighterpage.content, "html.parser")
+
+
+for name in soup.find_all('span', {"class": "b-content__title-highlight"}):
+  text = str(name.get_text())
+  text = text.strip()
+  fightername.append(text)
+for attribute in soup.find_all('li', {"class": "b-list__box-list-item_type_block"}):
+  text = str(attribute.get_text())
+  text = text.strip()
+  trimmed = re.search("(?s).*?Height:(.*)", text)
+  if trimmed is not None:
+    feet = ((trimmed.group(1)[:-4].strip()))
+    inches = ((trimmed.group(1)[-2].strip()))
+    totalheight = (int(feet) * 12 + int(inches))
+    fighterstat.append(totalheight)
+
+  trimmed = re.search("(?s).*Weight:(.*)", text)
+  if trimmed is not None:
+    fighterstat.append(int(trimmed.group(1)[:-4].strip()))
+  trimmed = re.search("(?s).*Reach:(.*)", text)
+  if trimmed is not None:
+    fighterstat.append(int(trimmed.group(1)[:-1].strip()))
+  trimmed = re.search("(?s).*STANCE:(.*)", text)
+  if trimmed is not None:
+    fighterstat.append((trimmed.group(1).strip()))
+  trimmed = re.search("(?s).*DOB:(.*)", text)
+  if trimmed is not None:
+    fighterstat.append(int(trimmed.group(1)[-4:].strip()))
+
+
+
+print(fightername)
+print(fighterstat)
+# END
+
+
+#  format and insert into mongo
+# for x in range(0, len(fightlinks)):
+
+#   # if x == 0:
+#   #   y = x
+#   # else:
+#   #   y += 1
+
+#   fight = {'_id': len(fightlinks)-x, 'url': fightlinks[x]}
+#   fightdata.append(fight)
+
+
+# print(fightdata)
+
+# client.ufcstats.fighterlinks.drop()
+# client.ufcstats.fighterlinks.insert_many(fighterdata)
+
+
+ #
+# for event in eventlinks:
+#   eventpage = requests.get(event)
+#   soup = BeautifulSoup(eventpage.content, "html.parser")
+#   for fightlink in soup.find_all('a'):
+#     href = str(fightlink.get('href'))
+#     match = re.search("(?P<url>https?://ufcstats.com/fight-details/.+)", href)
+#     if match is not None:
+#       fightlinks.append((match.group("url")))
+
+
+# print(fightlinks)
